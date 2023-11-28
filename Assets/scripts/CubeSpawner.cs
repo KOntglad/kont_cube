828e9bf4dc3ef68db1383dfcef83b68c4b041296
Baris <baris1.basol@gmail.com>
2023-11-28 14:34:11 +0300

    Added compiled DOTween
    also CubeObject script and CubeSpawner script created in this project

---
+++ Assets/scripts/CubeSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public Transform asdf;
+
+    private void Start()
+    {
+        SpawnCube();
+    }
+
+
+    void SpawnCube() 
+    {
+        CubeSpawn();
+    
+    }
+    
+    void CubeSpawn() 
+    { 
+    
+    
+    
+    }
+}
